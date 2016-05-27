ddd3d2cf51d44d6826e262a7b404664b68ed6037
tenstone <maolyc@gmail.com>
2016-05-27 17:47:08 +0800

    numpy方式输入数据，计算单根阳线的特征数据

--- klineyes/classifier/patterns/hammer.py
+++ klineyes/classifier/patterns/hammer.py
@@ -3,9 +3,9 @@
 
 hammer = {
     'hammer': {
-
+        "expression": lambda x: 'hammer_or_hanging' if x['top_height'] < 0.1 and x['bottom_height'] > x['entity_height'] * 2 and x['pct_amplitude'] >= 0.02 else False
     },
     'hanging': {
-
+        "expression": lambda x: 'hammer_or_hanging' if x['top_height'] < 0.1 and x['bottom_height'] > x['entity_height'] * 2 and x['pct_amplitude'] >= 0.02 else False
     }
 }
--- klineyes/classifier/single.py
+++ klineyes/classifier/single.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+def get_shadow_rate(data):
+    '''
+    单根蜡烛线 参数计算 top_height bottom_height entity_height pct_change pct_amplitude positive
+    :param data:
+    :return:
+    '''
+    negtive_filter = lambda x: None if x < 0 else x       # 过滤负值（无效数据）
+
+    calc_rate = lambda shadow_height, height : None if shadow_height is None or height == 0.0 or height is None else shadow_height / height     # 计算影线占实体的比例
+
+    height = data.highestPrice - data.lowestPrice   # 蜡烛图长度
+
+    return {
+        'top_height': calc_rate(negtive_filter(data.highestPrice - max(data.openPrice, data.closePrice)), height),
+        'bottom_height': calc_rate(negtive_filter(min(data.openPrice, data.closePrice)-data.lowestPrice), height),
+        'entity_height': calc_rate(abs(data.closePrice-data.openPrice), height),
+        'pct_change': calc_rate(abs(data.closePrice-data.lowestPrice), data.openPrice),
+        'pct_amplitude': calc_rate(abs(data.highestPrice-data.openPrice), data.openPrice),
+        'positive': True if((data.closePrice - data.preClosePrice) > 0) else False   # True=阳  False=阴
+    }
