332cce07a73a85c325e586ebeee153af2741f388
tenstone <maolyc@gmail.com>
2016-05-27 16:22:02 +0800

    star pattern expression

---
+++ klineyes/classifier/patterns/hammer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+hammer = {
+    'hammer': {
+
+    },
+    'hanging': {
+
+    }
+}
---
+++ klineyes/classifier/patterns/star.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+star = {
+    'normal': {
+        'name_zh': '十字星',
+        'feature': '',
+        'expression': lambda x: True if x['top_height'] >= 0.4 and x['bottom_height'] >= 0.4 and x['entity_height'] <= 0.02 else False,
+    },
+    't': {
+        'expression': lambda x: True if x['top_height'] < 0.05 and x['bottom_height'] > 0.3 and x['entity_height'] < 0.02 else False
+    },
+    'reverse_t': {
+        'expression': lambda x: True if x['top_height'] > 0.3 and x['bottom_height'] < 0.03 and x['entity_height'] < 0.02 else False
+    },
+    'long': {
+        'expression': lambda x: True if x['top_height'] >= 0.4 and x['bottom_height'] >= 0.4 and x['entity_height'] <= 0.08 and x['pct_amplitude'] >= 0.03 else False
+    },
+    'shooting': {
+        'expression': lambda x: True if x['top_height'] > 0.5 and x['bottom_height'] >= 0.1 and x['entity_height'] >= 0.1 and x['pct_amplitude'] >= 0.03 else False
+    }
+}
