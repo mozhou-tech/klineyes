6be2d9ac88394c5509a55a0aef3de9c83e7a32c4
tenstone <maolyc@gmail.com>
2016-06-07 15:41:53 +0800

    缓存目录结构

--- scripts/debug_markets.py
+++ scripts/debug_markets.py
@@ -3,7 +3,7 @@
 
 import klineyes.markets.stocks.get_realtime as get_realtime
 import tushare as ts
-print ts.get_hist_data(code='600496', ktype='30').index
+print ts.get_hist_data(code='600496', start='2016-06-01', end='2016-06-09', ktype='30')
 
 
 
